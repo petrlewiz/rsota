5f9c39e55c4e13e5eb1976e85468300b2d2b9b20
Peter Lewis <lewis.peter@live.ca>
2026-09-02 02:53:08 +0000

    Make card a clickable link, and add professor_data

---
+++ professor_data.py
@@ -0,0 +1,16 @@
+professor = [
+    {
+        "name": "Rev. Dennis Lewis",
+        "bio" : """Rev. Dennis Lewis, ordained as a Licensed Minister in 2011, is currently the Vice-President of Rhema Studies of Theology Association (RSOTA) and serves on the teaching faculty of RSOTA Biblical Training.
+
+Rev. Lewis is a graduate of RSOTA Biblical Training with a Certificate (2008) and Diploma (2010). In addition, he has received his Bachelor of Theology (2010) from California State Christian University and a Masters of Theology (2012) from Mount Olive Bible Institute and Seminary.
+
+He is an active speaker in churches across the GTA, which fuels his passion for the Word of God... to study it, to live it, and to expound it.
+
+Finally, and most importantly, God has blessed him richly with a beautiful wife, and three lovely daughters. To God be all the glory. """ 
+    }
+
+    {
+        
+    }
+]
